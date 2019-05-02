dc17238f8b025e461874fef4bcab8c72b4154cbf
Yu-Fan Tung <yufantung@gmail.com>
2019-05-02 20:26:39 +0800

    cm

---
+++ standard.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Custom sorting
+# ls.sort(key=lambda x: x.foo)
+
+# Empty list
+# [None] * 10
+
+""" Returns a string """
+def solve():
+    return ''
+
+if __name__ == '__main__':
+    T = int(input())
+    for caseIndex in range(1, T + 1):
+        print ('Case #{}: {}'.format(caseIndex, solve()))
+
